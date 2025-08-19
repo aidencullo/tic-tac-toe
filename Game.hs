92a664c54d3019bd5589e680c1aaec45c2266ec7
aidencullo <aidencullo@gmail.com>
2025-08-19 11:14:24 -0400

    Rename showGameState to showBoard for better clarity

--- Game.hs
+++ Game.hs
@@ -2,8 +2,8 @@ module Game where
 
 import Board
   
-showGameState :: Maybe Int -> IO ()
-showGameState state = printBoard emptyBoard
+showBoard :: IO ()
+showBoard = printBoard emptyBoard
 
 type Move = String
 
@@ -13,24 +13,8 @@ getMove = do
   input <- getLine
   return input
 
-state :: Maybe Int
-state = Nothing
-
 play :: IO ()
 play = do
-  start
-
-start :: IO ()
-start = do
-  evolve state
-  return ()
-
-processMove :: Maybe Int -> String -> Maybe Int
-processMove state move = Nothing
-
-evolve :: Maybe Int -> IO (Maybe Int)
-evolve state = do
   move <- getMove
-  let newState = processMove state move
-  showGameState newState
-  evolve newState
+  print move
+  showBoard

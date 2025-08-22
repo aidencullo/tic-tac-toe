56fcb981d80392134f83a205d4a016020db7c9f7
aidencullo <aidencullo@gmail.com>
2025-08-22 14:54:26 -0400

    chore: push current changes

--- Board.hs
+++ Board.hs
@@ -14,12 +14,18 @@ displayBoard state = do
   putStrLn boardStr
   printBuffer
 
+printBoard :: String -> IO ()
+printBoard board = putStrLn board
+
+createBoard :: State -> String
 createBoard state = createRow state
 
+createRow :: State -> String
 createRow [] = ""
 createRow [x, y, z] = createCell [x, y, z]
 createRow (x:y:z:xs) = createCell [x, y, z] ++ "\n--------\n" ++ createRow xs
 
+createCell :: [Int] -> String
 createCell [] = ""
 createCell [x] = show x
 createCell (x:xs) = show x ++ "|" ++ createCell xs

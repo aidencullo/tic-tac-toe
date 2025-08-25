ca0d50cb565020351542096cc2fdc5efde533acf
aidencullo <aidencullo@gmail.com>
2025-08-25 16:31:36 -0400

    chore: push current changes

--- Game.hs
+++ Game.hs
@@ -2,27 +2,28 @@ module Game where
 
 import Board
 import Types
-
 import Text.Read (readMaybe)
 
+-- Conversion
 strToInt :: String -> Move
 strToInt s = read s
 
+-- Input
+promptUser :: IO String
+promptUser = getLine
+
+enterMove :: IO Move
 enterMove = do
     putStrLn "Enter your move: "
     moveString <- promptUser
     let move = strToInt moveString
     return move
 
-promptUser :: IO String
-promptUser = getLine
-
+-- Game state
 initialState :: State
 initialState = replicate 9 0
 
-start :: IO State
-start = evolve initialState
-
+-- Processing
 process :: State -> Move -> State
 process state move = sanitizeMove move `seq` initialState
 
@@ -32,13 +33,18 @@ sanitizeMove 1 = ()
 sanitizeMove 2 = ()
 sanitizeMove _ = error "Invalid move"
 
-
+-- Output
 printState :: State -> IO ()
 printState state = displayBoard state
 
+-- Game loop
 evolve :: State -> IO State
 evolve state = do
-  move <- enterMove
-  let newState = process state move
-  printState newState
-  evolve newState
+    move <- enterMove
+    let newState = process state move
+    printState newState
+    evolve newState
+
+-- Entry point
+start :: IO State
+start = evolve initialState
